f52f26eca5451c7be98ae30953d6686b326dbcbe
mihait29 <tmihaigabriel97@gmail.com>
2022-06-24 16:04:26 +0200

    Loss function added; a short start on NN

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import re
+#import tenserflow
+from keras.preprocessing.text import Tokenizer
 import nltk
 import pandas as pd
 from nltk import WordNetLemmatizer
@@ -7,18 +9,22 @@ from nltk.corpus import wordnet
 import json
 import string
 import pandas as pd
-import re
-
 from tqdm import tqdm
 from nltk.collocations import TrigramCollocationFinder, TrigramAssocMeasures
 from unidecode import unidecode
 
 nlp = WordNetLemmatizer()
+positive_lines = list()
+negative_lines = list()
 
 
-def print_hi(name):
-	# Use a breakpoint in the code line below to debug your script.
-	print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def count_loss_function(y_actual, y_predicted):
+	if y_actual == y_predicted:
+		loss_value = 0
+	else:
+		loss_value = 1
+	return loss_value
+
 
 
 def get_wordnet_pos(word):
@@ -84,27 +90,36 @@ def dictionary(file, dic):
 			dic[word] = 1
 		else:
 			dic[word] = dic[word]+1
-	# dic2 = dict(sorted(dic.items(),reverse=True, key=lambda x: x[1]))
-	# for key in list(dic2.keys()):
-	# 	with open("counts-dracula.txt", "a") as f:
-	# 		print(key, '\t', dic2[key], file = f)
-	# f.close()
 
 
-def writeToFile(dictionary):
+def writeToFile(dictionary,name):
 	dic = dict(sorted(dictionary.items(), reverse=True, key = lambda x: x[1]))
 	for key in list(dic.keys()):
-		with open("vocabulary.txt", "a") as f:
+		with open(name+".txt", "a") as f:
 			print(key, '\t', dic[key], file = f)
 	f.close()
+
+
+def process_docs(text):
+	for i in range(0,10):
+		file = text["review"][i]
+		tokens = clean_doc(file)
+		line = ' '.join(tokens)
+		if text["sentiment"][i] == "positive":
+			positive_lines.append(line)
+		else:
+			negative_lines.append(line)
 
 # load the document
 col_list = ["review", "sentiment"]
 text = pd.read_csv("IMDB Dataset.csv", usecols=col_list)
+process_docs(text)
 dic = dict()
-for i in range(0,6000):
+lines = list()
+for i in range(0,10):
 	file = text["review"][i]
 	tokens = clean_doc(file)
+	line = ' '.join(tokens)
 	lemmas = []
 	for w in tokens:
 		w = nlp.lemmatize(w, get_wordnet_pos(w))
@@ -113,16 +128,17 @@ for i in range(0,6000):
 # remove word with low occurrence
 min_frequency = 2
 final_dic = dict()
-for word in dic:
-	if dic[word] >= min_frequency:
-		final_dic[word] = dic[word]
+# for word in dic:
+# 	if dic[word] >= min_frequency:
+# 		final_dic[word] = dic[word]
+#writeToFile(final_dic, "vocabulary")
 
-writeToFile(final_dic)
+tokenizer = Tokenizer()
 
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-	print_hi('PyCharm')
+# fit the tokenizer on the documents
+docs = positive_lines + negative_lines
+tokenizer.fit_on_texts(docs)
+Xtrain = tokenizer.texts_to_matrix(docs, mode='freq')
+print(Xtrain.shape)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
